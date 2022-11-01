daca8121f55dd6800fd7236f0e4a0bb72281b7e5
Jonatas Taveira <jtaveira@gmail.com>
2022-10-31 23:15:14 -0500

    primeiros componetentes

---
+++ src/components/Form/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { View, Text, TextInput, Button} from "react-native"
+
+export default function Form(){
+    return(
+        <View>
+          <View>
+            <Text>Altura</Text>
+                <TextInput
+                    placeholder="Ex.: 1.75"
+                    keyboardType="numeric"
+                />
+            <Text>Peso</Text>
+                <TextInput
+                    placeholder="Ex.: 65.500"
+                    keyboardType="numeric"
+                />
+                <Button
+                    title="Calcular IMC"    
+                />
+          </View>
+          {/* <ResultImc messageResultImc={messageImc} resultImc={imc}/> */}
+        </View>
+    );
+}
